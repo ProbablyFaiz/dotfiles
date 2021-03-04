8dc91d05095f6bdff3f5702547ed7fab9d70c279
Faiz Surani <faiz.surani@gmail.com>
2021-03-03 19:14:26 -0800

    Add highlighting for less

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,13 @@ alias config='/usr/bin/git --git-dir=/home/faiz/.dotfiles/ --work-tree=/home/fai
 thefuck --alias | source 
 source ~/.asdf/asdf.fish
 
+set -x LESS_TERMCAP_mb (printf "\033[1;32m")  
+set -x LESS_TERMCAP_md (printf "\033[1;32m")  
+set -x LESS_TERMCAP_me (printf "\033[0m")  
+set -x LESS_TERMCAP_se (printf "\033[0m")  
+set -x LESS_TERMCAP_so (printf "\033[1;33m")  
+set -x LESS_TERMCAP_ue (printf "\033[0m")
+set -x LESS_TERMCAP_us (printf "\033[1;4;31m")  
+
 export RAILS_DB_HOST=localhost
 export GPG_TTY=(tty)
