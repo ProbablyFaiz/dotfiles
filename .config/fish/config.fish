0aad64f69be83fd0b3123a4e23a7f3e835c8430f
Faiz Surani <faiz.surani@gmail.com>
2024-02-21 13:25:36 -0800

    Set aliases/starship as interactive-only

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,32 @@
-starship init fish | source
-alias ls="lsd -A"
-alias gloh='git log --oneline -n 10'
-alias asrp='asdf reshim python'
-alias wh='wormhole'
-alias py='bpython'
-alias efish='source ~/.config/fish/config.fish'
-alias vifish='vi ~/.config/fish/config.fish'
-alias gofish='vi ~/.config/fish/config.fish; source ~/.config/fish/config.fish'
-alias venv='source venv/bin/activate.fish'
-alias dvenv='deactivate'
-alias bi='brew install'
-alias bic='brew install --cask'
-alias bs='brew search'
-alias g='git'
-alias pi='pip install'
-alias pipir='pip install -r requirements.txt'
-alias pipu='pip install --upgrade'
-alias pipup='pip install --upgrade pip'
-alias gitconfig='vi ~/.gitconfig'
-alias untar='tar -xvzf'
-alias countloc='git ls-files | grep ".*\.\(ts\|tsx\|js\|jsx\|py\|md\)\$" | grep -v "webpack" | xargs wc -l | sort -n'
+if status is-interactive
+    starship init fish | source
+    alias ls="lsd -A"
+    alias gloh='git log --oneline -n 10'
+    alias asrp='asdf reshim python'
+    alias wh='wormhole'
+    alias py='bpython'
+    alias efish='source ~/.config/fish/config.fish'
+    alias vifish='vi ~/.config/fish/config.fish'
+    alias gofish='vi ~/.config/fish/config.fish; source ~/.config/fish/config.fish'
+    alias venv='source venv/bin/activate.fish 2>/dev/null || source .venv/bin/activate.fish'
+    alias dvenv='deactivate'
+    alias bi='brew install'
+    alias bic='brew install --cask'
+    alias bs='brew search'
+    alias g='git'
+    alias pi='pip install'
+    alias pipir='pip install -r requirements.txt'
+    alias pipu='pip install --upgrade'
+    alias pipup='pip install --upgrade pip'
+    alias gitconfig='vi ~/.gitconfig'
+    alias untar='tar -xvzf'
+    alias countloc='git ls-files | grep ".*\.\(ts\|tsx\|js\|jsx\|py\|md\)\$" | grep -v "webpack" | xargs wc -l | sort -n'
+    alias ppython='PYTHONPATH=. python $argv'
 
-function ppid
-    lsof -ti:$argv
-end
+    function ppid
+        lsof -ti:$argv
+    end
+ end
 
 switch (uname)
 	case Linux
@@ -40,6 +43,7 @@ switch (uname)
                 fish_add_path /opt/homebrew/opt/llvm@11/bin
                 fish_add_path '/Applications/Racket v8.3/bin'
                 fish_add_path '/Users/faiz/.local/bin'
+                fish_add_path '/Users/faiz/.cargo/bin'
                 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 	end
 
@@ -54,7 +58,6 @@ set -x LESS_TERMCAP_us (printf "\033[1;4;31m")
 fish_vi_key_bindings
 
 export GPG_TTY=(tty)
-
 
 #set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /Users/faiz/.ghcup/bin # ghcup-env
 
