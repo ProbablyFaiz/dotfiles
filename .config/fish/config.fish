02e800e2bdab290634eb594acdc43e2faba401cf
Faiz Surani <faiz.surani@gmail.com>
2024-08-23 12:04:59 -0700

    Path handling

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,10 @@
 if status is-interactive
     starship init fish | source
+    alias u="uv run"
     alias ls="lsd -A"
     alias gloh='git log --oneline -n 10'
     alias asrp='asdf reshim python'
-    alias wh='wormhole'
+    alias wh='wormhole --transit-helper=tcp:relay.soc220014.projects.jetstream-cloud.org:4001'
     alias py='bpython'
     alias efish='source ~/.config/fish/config.fish'
     alias vifish='vi ~/.config/fish/config.fish'
@@ -17,11 +18,13 @@ if status is-interactive
     alias ucom='uv pip compile -o requirements.txt requirements.in'
     alias upi='uv pip install'
     alias upir='uv pip install -r requirements.txt'
+    alias upied='uv pip install -e .'
     alias pi='pip install'
     alias pipir='pip install -r requirements.txt'
     alias pipu='pip install --upgrade'
     alias pipup='pip install --upgrade pip'
-    alias gitconfig='vi ~/.gitconfig'
+    alias pcr='pre-commit run --all-files'
+    alias gcon='vi ~/.gitconfig'
     alias untar='tar -xvzf'
     alias countloc='git ls-files | grep ".*\.\(ts\|tsx\|js\|jsx\|py\|md\)\$" | grep -v "webpack" | xargs wc -l | sort -n'
     alias ppython='PYTHONPATH=. python $argv'
@@ -32,27 +35,29 @@ if status is-interactive
  end
 
 switch (uname)
-	case Linux
-                fish_add_path '/home/faiz/.local/bin'
-                fish_add_path '/home/faiz/.cargo/bin'
-                source "$HOME/.cargo/env.fish"
-        	source ~/.asdf/asdf.fish 2> /dev/null
-                alias config='/usr/bin/git --git-dir=/home/faiz/.cfg/ --work-tree=/home/faiz'
-                # opam configuration
-                source /home/faiz/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-                if test -f /home/faiz/.autojump/share/autojump/autojump.fish; . /home/faiz/.autojump/share/autojump/autojump.fish; end
-	case Darwin
-                set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/faiz/.ghcup/bin $PATH # ghcup-env
-                set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+    case Linux
+        set -gx PATH /home/faiz/.local/bin $PATH
+        set -gx PATH /home/faiz/.cargo/bin $PATH
+        source ~/.asdf/asdf.fish 2>/dev/null
+        alias config='/usr/bin/git --git-dir=/home/faiz/.dotfiles/ --work-tree=/home/faiz'
+        # opam configuration
+        source /home/faiz/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
 
-		source /opt/homebrew/opt/asdf/libexec/asdf.fish
-                alias config='/usr/bin/git --git-dir=/Users/faiz/.cfg --work-tree=/Users/faiz'
-                fish_add_path /opt/homebrew/opt/llvm@11/bin
-                fish_add_path '/Applications/Racket v8.3/bin'
-                fish_add_path '/Users/faiz/.local/bin'
-                fish_add_path '/Users/faiz/.cargo/bin'
-                [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
-	end
+    case Darwin
+        set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+        set -gx PATH $HOME/.cabal/bin /Users/faiz/.ghcup/bin $PATH # ghcup-env
+        set -gx PATH /opt/homebrew/bin $PATH
+
+        source /opt/homebrew/opt/asdf/libexec/asdf.fish
+        alias config='/usr/bin/git --git-dir=/Users/faiz/.cfg --work-tree=/Users/faiz'
+        set -gx PATH /opt/homebrew/opt/llvm@11/bin $PATH
+        set -gx PATH '/Applications/Racket v8.3/bin' $PATH
+        set -gx PATH /Users/faiz/.local/bin $PATH
+        set -gx PATH /Users/faiz/.cargo/bin $PATH
+        test -f /opt/homebrew/share/autojump/autojump.fish; and source /opt/homebrew/share/autojump/autojump.fish
+        set -gx PATH /opt/homebrew/opt/python@3.11/bin $PATH
+        set -U fish_user_paths "/Users/faiz/Library/Application Support/uv/python/cpython-3.11.9-macos-aarch64-none/bin" $fish_user_paths
+    end
 
 set -x LESS_TERMCAP_mb (printf "\033[1;32m")  
 set -x LESS_TERMCAP_md (printf "\033[1;32m")  
@@ -65,9 +70,3 @@ set -x LESS_TERMCAP_us (printf "\033[1;4;31m")
 fish_vi_key_bindings
 
 export GPG_TTY=(tty)
-
-#set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /Users/faiz/.ghcup/bin # ghcup-env
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/faiz/dl/google-cloud-sdk/path.fish.inc' ]; . '/Users/faiz/dl/google-cloud-sdk/path.fish.inc'; end
