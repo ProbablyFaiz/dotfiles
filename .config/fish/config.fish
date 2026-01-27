18a3b41c1a531c1f0e2c1e7f92ced25f1fa065ad
Faiz Surani <faiz.surani@gmail.com>
2026-01-27 13:14:13 -0800

    Enhance

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,19 @@
 if status is-interactive
     starship init fish | source
     alias u="uv run"
+    alias c="claude --dangerously-skip-permissions"
     alias jt="just"
     alias pn="pnpm"
     alias ls="lsd -A"
     alias cless="csvlens"
     alias gloh='git log --oneline -n 10'
+    alias grao='git remote add origin'
     alias wh='wormhole --transit-helper=tcp:relay.soc220014.projects.jetstream-cloud.org:4001'
     alias py='bpython'
     alias efish='source ~/.config/fish/config.fish'
     alias vifish='vi ~/.config/fish/config.fish'
     alias gofish='vi ~/.config/fish/config.fish; source ~/.config/fish/config.fish'
-    alias venv='source venv/bin/activate.fish 2>/dev/null || source .venv/bin/activate.fish'
+    alias venv='source .venv/bin/activate.fish 2>/dev/null || source backend/.venv/bin/activate.fish'
     alias dvenv='deactivate'
     alias bi='brew install'
     alias bic='brew install --cask'
@@ -31,7 +33,7 @@ if status is-interactive
     alias pcra='git aa && pre-commit run --all-files'
     alias gcon='vi ~/.gitconfig'
     alias untar='tar -xvzf'
-    alias countloc='git ls-files | grep ".*\.\(ts\|tsx\|js\|jsx\|py\|md\)\$" | grep -v "webpack" | xargs wc -l | sort -n'
+    alias countloc='git ls-files | grep ".*\.\(ts\|tsx\|js\|jsx\|py\|md\|css\|fish\|bash\|sh\|conf\|ini\|ya?ml\)\$" | grep -v "webpack" | grep -v ".gen.ts" | grep -v "pnpm-lock" | grep -v "/components/ui/" | xargs wc -l | sort -n'
     alias ppython='PYTHONPATH=. python $argv'
     alias ta='tmux a'
     alias tat='tmux a -t'
@@ -42,6 +44,7 @@ if status is-interactive
     alias dcu='docker compose up'
     alias dcd='docker compose down'
     alias dcl='docker compose logs'
+    alias cl='claude'
     alias s='ssh poirot'
 
     function ppid
@@ -62,12 +65,12 @@ switch (uname)
         set -gx PATH /home/faiz/scripts $PATH
         set -Ux RESTIC_PASSWORD_FILE /home/faiz/.config/restic/r2.password
         set -Ux RESTIC_REPOSITORY rclone:r2:/faiz/backups
-        source ~/.asdf/asdf.fish 2>/dev/null
         alias config='/usr/bin/git --git-dir=/home/faiz/.cfg/ --work-tree=/home/faiz'
         alias cfg='config'
         # opam configuration
         source /home/faiz/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
         if test -f /home/faiz/.autojump/share/autojump/autojump.fish; . /home/faiz/.autojump/share/autojump/autojump.fish; end
+        eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
         # pnpm
         set -gx PNPM_HOME "/home/faiz/.local/share/pnpm"
@@ -79,6 +82,8 @@ switch (uname)
         if test -e /home/faiz/.config/fish/private.fish
             source /home/faiz/.config/fish/private.fish
         end
+        zoxide init fish | source
+        direnv hook fish | source
 
     case Darwin
         set -gx PNPM_HOME "/Users/faiz/.local/share/pnpm"
@@ -113,6 +118,9 @@ set -x LESS_TERMCAP_so (printf "\033[1;33m")
 set -x LESS_TERMCAP_ue (printf "\033[0m")
 set -x LESS_TERMCAP_us (printf "\033[1;4;31m")  
 
+set -x CLAUDE_CODE_DISABLE_FEEDBACK_SURVEY 1
+set -x CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC 1
+
 fish_vi_key_bindings
 
 export GPG_TTY=(tty)
@@ -125,3 +133,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/faiz/google-cloud-sdk/path.fish.inc' ]; . '/home/faiz/google-cloud-sdk/path.fish.inc'; end
